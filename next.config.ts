a191e1c3b4911b94537492b5c34af9c58316592c
Akzi <akzeineperkin@gmail.com>
2026-06-18 08:12:00 -0700

    ignore build errors + trigger rebuild

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src https://www.youtube.com https://youtube.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
